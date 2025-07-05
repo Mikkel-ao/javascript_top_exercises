460a302e9faa7162787135f460c553cbf1c4bed1
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-07-06 00:52:52 +0200

    task 07 completed

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (typeof str !== 'string') {
+        return 'Input must be a string';
+    }
+    
+    // Reverse the string using split, reverse, and join methods
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
